c8d30fd1f7502193c82c9c775e87a990715b6fd9
AyakorK <guillaume.moret@yahoo.com>
2023-05-22 15:03:15 +0200

    Add the content_block to the homepage

---
+++ app/cells/decidim/homepage_proposals/content_blocks/proposals_slider_cell.rb
@@ -0,0 +1,10 @@
+# frozen_string literal: true
+
+module Decidim
+  module HomepageProposals
+    module ContentBlocks
+      class ProposalsSliderCell < Decidim::ViewModel
+      end
+    end
+  end
+end
--- lib/decidim/homepage_proposals/engine.rb
+++ lib/decidim/homepage_proposals/engine.rb
@@ -18,6 +18,20 @@ module Decidim
       initializer "HomepageProposals.webpacker.assets_path" do
         Decidim.register_assets_path File.expand_path("app/packs", root)
       end
+
+      initializer "decidim_homepage_proposals.add_cells_view_paths" do
+        Cell::ViewModel.view_paths << File.expand_path("#{Decidim::HomepageProposals::Engine.root}/app/cells")
+        Cell::ViewModel.view_paths << File.expand_path("#{Decidim::HomepageProposals::Engine.root}/app/views") # for proposal partials
+      end
+
+
+      initializer "decidim_homepage_proposals.content_blocks" do
+        Decidim.content_blocks.register(:homepage, :proposals_slider) do |content_block|
+          content_block.cell = "decidim/homepage_proposals/content_blocks/proposals_slider"
+          content_block.public_name_key = "decidim.homepage_proposals.content_blocks.proposals_slider_cell.rb.name"
+        end
+      end
     end
+
   end
 end
