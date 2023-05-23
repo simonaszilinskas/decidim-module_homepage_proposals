bd977146af87b5195bffc36254211ccf55261385
AyakorK <guillaume.moret@yahoo.com>
2023-05-23 12:13:30 +0200

    Modify the css of the slider to make the arrows appear on the screen

--- app/packs/entrypoints/decidim_homepage_proposals.js
+++ app/packs/entrypoints/decidim_homepage_proposals.js
@@ -1,4 +1,7 @@
 // Images
 require.context("../images", true)
 
+
 import "src/decidim/homepage_proposals/admin"
+import "./decidim/homepage_proposals/slider"
+import "stylesheets/decidim/homepage_proposals/slider.scss"
