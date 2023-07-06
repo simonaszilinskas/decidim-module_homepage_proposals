416ca148c1f703d48862862349f3662fabce47de
quentinchampenois <quentin.champenois7@gmail.com>
2023-07-06 17:36:26 +0200

    Add getter setter for pervView and Breakpoints

--- app/packs/src/decidim/homepage_proposals/glideBuilder.js
+++ app/packs/src/decidim/homepage_proposals/glideBuilder.js
@@ -4,13 +4,54 @@ export default class GlideBuilder {
     constructor(selector = '.glide', type = 'carousel') {
         this.type = type
         this.setOpts()
+        this.pervView = GlideBuilder.defaultPervView();
+        this.breakpoints = GlideBuilder.defaultBreakpoints();
         this.glide = new Glide(selector, this.options)
 
         this.bindings()
     }
 
-    static pervView() {
-        return 3;
+    static defaultPervView() {
+        return 4;
+    }
+
+    get perView() {
+        return this.pervView;
+    }
+
+    // Set pervView, must be between 1 and 4 excluded
+    // Not valid when :
+    // * pervView < 1 : Then placeholder is added at ./glideItems/Manager.js:122
+    // * pervView > 4 : Then max pervView must be 4
+    set pervView(pervView) {
+        if (pervView > 0 && pervView < 4) {
+            this.pervView = pervView;
+        }
+    }
+
+    static defaultBreakpoints() {
+        return { 1024: { perView: 3 }, 768: { perView: 2 }, 480: { perView: 1 } };
+    }
+
+    get breakpoints() {
+        return this.breakpoints;
+    }
+
+    set breakpoints(pervView) {
+        if (pervView > 0 && pervView < 4) {
+            let breakpoints;
+            switch (pervView) {
+                case 2:
+                    breakpoints = { 1024: {perView: 2}, 768: {perView: 2}, 480: {perView: 1} };
+                    break;
+                case 1:
+                    breakpoints = { 1024: {perView: 1}, 768: {perView: 1}, 480: {perView: 1} };
+                    break;
+                default:
+                    breakpoints = GlideBuilder.defaultBreakpoints();
+            }
+            this.breakpoints = breakpoints;
+        }
     }
 
     destroy() {
@@ -40,11 +81,9 @@ export default class GlideBuilder {
             type: this.type,
             startAt: 0,
             autoplay: 0,
-            perView: GlideBuilder.pervView(),
+            perView: this.pervView,
+            breakpoints: this.breakpoints,
             hoverpause: true,
-            breakpoints: {
-                1024: { perView: 3 }, 768: { perView: 2 }, 480: { perView: 1 }
-            },
             perTouch: 1
         }
     }
