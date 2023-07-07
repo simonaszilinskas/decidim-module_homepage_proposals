9a7196e6b675b7c94e455e4faed64e2888e9f400
quentinchampenois <quentin.champenois7@gmail.com>
2023-07-07 11:13:12 +0200

    Fix scrolling on placeholders when items number is inferior to default pervView (4 items)

--- app/packs/src/decidim/homepage_proposals/glideBuilder.js
+++ app/packs/src/decidim/homepage_proposals/glideBuilder.js
@@ -1,57 +1,52 @@
 import Glide from "@glidejs/glide";
 
+// Create a GlideJS carousel
+// See documentation: https://glidejs.com/docs/
 export default class GlideBuilder {
-    constructor(selector = '.glide', type = 'carousel') {
-        this.type = type
-        this.setOpts()
-        this.pervView = GlideBuilder.defaultPervView();
-        this.breakpoints = GlideBuilder.defaultBreakpoints();
-        this.glide = new Glide(selector, this.options)
-
-        this.bindings()
-    }
-
     static defaultPervView() {
         return 4;
-    }
-
-    get perView() {
-        return this.pervView;
-    }
-
-    // Set pervView, must be between 1 and 4 excluded
-    // Not valid when :
-    // * pervView < 1 : Then placeholder is added at ./glideItems/Manager.js:122
-    // * pervView > 4 : Then max pervView must be 4
-    set pervView(pervView) {
-        if (pervView > 0 && pervView < 4) {
-            this.pervView = pervView;
-        }
     }
 
     static defaultBreakpoints() {
         return { 1024: { perView: 3 }, 768: { perView: 2 }, 480: { perView: 1 } };
     }
 
-    get breakpoints() {
-        return this.breakpoints;
+    constructor(selector = '.glide', type = 'carousel', pervView = GlideBuilder.defaultPervView()) {
+        this.type = type
+        this.pervView = this.setPervView(pervView);
+        this.breakpoints = this.setBreakpoints();
+        this.setOpts()
+        this.glide = new Glide(selector, this.options)
+
+        this.bindings()
     }
 
-    set breakpoints(pervView) {
+    // Set pervView, must be between 0 and 4 excluded
+    // Returns default pervView when :
+    // * pervView < 1 : Placeholder is added at ./glideItems/Manager.js:122
+    // * pervView > 4 : Max pervView must be 4
+    setPervView(pervView) {
         if (pervView > 0 && pervView < 4) {
-            let breakpoints;
-            switch (pervView) {
-                case 2:
-                    breakpoints = { 1024: {perView: 2}, 768: {perView: 2}, 480: {perView: 1} };
-                    break;
-                case 1:
-                    breakpoints = { 1024: {perView: 1}, 768: {perView: 1}, 480: {perView: 1} };
-                    break;
-                default:
-                    breakpoints = GlideBuilder.defaultBreakpoints();
-            }
-            this.breakpoints = breakpoints;
+            return pervView;
+        } else {
+            return GlideBuilder.defaultPervView();
         }
+    }
+
+    // Glide breakpoints must be accorded to the pervView to prevent scrolling on placeholders
+    setBreakpoints() {
+        let breakpoints;
+        switch (this.pervView) {
+            case 2:
+                breakpoints = { 1024: {perView: 2}, 768: {perView: 2}, 480: {perView: 1} };
+                break;
+            case 1:
+                breakpoints = { 1024: {perView: 1}, 768: {perView: 1}, 480: {perView: 1} };
+                break;
+            default:
+                breakpoints = GlideBuilder.defaultBreakpoints();
+        }
+        return breakpoints;
     }
 
     destroy() {
